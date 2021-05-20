de295882b374b2499e23f59faa66e88a467d7d84
omkar firame <omkarfirame@gmail.com>
2021-05-20 14:30:04 +0530

    third commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipie.model';
+import { Recipe } from '../recipe.model';
 
 
 @Component({
@@ -8,7 +8,7 @@ import { Recipe } from '../recipie.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipe:Recipe[]  = [
+  recipes:Recipe[]  = [
     new Recipe('A test recipe','This is a test','https://www.simplyrecipes.com/thmb/2Ggz7Ua9qkBAfVfl4FGh2wlNeBc=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Quesadilla-LEAD-5-55da42a2a306497c85b1328385e44d85.jpg'),
     new Recipe('A test recipe','This is a test','https://www.simplyrecipes.com/thmb/2Ggz7Ua9qkBAfVfl4FGh2wlNeBc=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Quesadilla-LEAD-5-55da42a2a306497c85b1328385e44d85.jpg')
     
